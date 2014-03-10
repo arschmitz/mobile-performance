d01b6088e58f5a606f974e83a53656f4af8c0eb4
Alexander Schmitz <arschmitz@gmail.com>
2014-03-10 17:56:26 -0400

    Added new presentation for jquery-foundation

--- body.php
+++ body.php
@@ -6,13 +6,28 @@
 			echo Minify_HTML::minify( file_get_contents(  "popup.html" ) );
 		}
 		$files = scandir('./slides');
+		$subfilesSorted;
 		foreach( $files as $file ){
-			if( $file !== "." && $file !== ".." ) {
+			if( $file !== "." && $file !== ".." && !is_dir( "./slides/".$file ) ) {
 				$index = intVal( substr( $file, 0, 2 ) );
 				$filesSorted[ $index ] = $file;
+			} else if ( is_dir( "./slides/".$file ) && $file !== "." && $file !== ".." ) {
+				$subfolder = scandir( "./slides/".$file );
+				foreach( $subfolder as $subfile ){
+					if( $subfile !== "." && $subfile !== ".." ) {
+						$index = intVal( substr( $subfile, 0, 2 ) );
+						$subfilesSorted[ $index ] = $file."/".$subfile;
+					}
+				}
 			}
 		}
-		ksort($filesSorted);
+
+		ksort( $filesSorted );
+		krsort( $subfilesSorted );
+		foreach( $subfilesSorted as $file ) {
+			array_unshift( $filesSorted, $file );
+		}
+		print_r( $filesSorted );
 		$panel =  "<div class='preso-panel' id='slide-list'>";
 		$panel .= "<ol class='preso-panel-list ui-mini ui-listview'>";
 		$panel .= "<li class='ui-li-divider ui-bar-inherit ui-first-child'>Table of Contents</li>";
@@ -20,16 +35,26 @@
 		$length = count( $filesSorted );
 		$count = 0;
 		$slides = "";
+		$folder = "";
+		$perf = false;
 		foreach($filesSorted as $file){
 			if( $file !== "." && $file !== ".." ) {
 				$slides .= file_get_contents( "./slides/".$file );
 				$count++;
+				$match = preg_match( "`(.)*/`", $file, $matches );
+				if( $match && $matches[0] !== $folder ){
+					$folder = $matches[0];
+					$panel .= "<li class='ui-li-divider ui-bar-a'><h1>".preg_replace( "/jquery/","jQuery", preg_replace( "`-|/`", " ",$folder) )."</h1></li>";
+				} else if ( !$match && !$perf ) {
+					$perf = true;
+					$panel .= "<li class='ui-li-divider ui-bar-a'><h1>Mobile Performance</h1></li>";
+				}
 				if( $count !== $length ){
 					$panel .= "<li>";
 				} else {
 					$panel .= "<li class='ui-last-child'>";
 				}
-				$panel .= "<a class='ui-btn ui-btn-icon-right ui-icon-carat-r' href='#".preg_replace( "/\.html/", "", $file )."'>".ucwords( preg_replace( "/-/", " ", preg_replace('/\.html|^([0-9])*/', '', $file ) ) )."</a>";
+				$panel .= "<a class='ui-btn ui-btn-icon-right ui-icon-carat-r' href='#".preg_replace( "`(.)*/`", "", preg_replace( "/\.html/", "", $file ) )."'>".ucwords( preg_replace( "/^([0-9])*/", "", preg_replace( "`^(.)*/`", " ", preg_replace("`/(.)*/`", "", preg_replace( "/-/", " ", preg_replace('/\.html|^([0-9])*/', '', $file ) ) ) ) ) )."</a>";
 				$panel .= "</li>";
 			}
 		}
